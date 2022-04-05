0fa214251c2e2d2930c77564bcb2d7f44441281b
rudemoos <m.bloemendaal210@gmail.com>
2022-04-05 14:51:01 +0200

    dgh

--- store.js
+++ store.js
@@ -1,3 +1,3 @@
 $('div').on('click', function () {
     
-});
+});;kjn
--- winkelmand.js
+++ winkelmand.js
@@ -1,3 +1,6 @@
+let product = 0;
+
 $('div').on('click', function(){
-    
+    product += 1;
+        $('#divResult').text(product);
 })
